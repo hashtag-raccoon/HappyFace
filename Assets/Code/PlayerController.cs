8c82e52a4d5280d156d5126b143600eb82ade8e6
dharmaflash <dharmaflash1@gmail.com>
2022-05-23 13:17:53 +0900

    ㄻㅅㄷㅊ
    ㅇㅇ

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerControllwe : MonoBehaviour
 {
+    int speed = 6;
+
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float playerMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            playerMove = speed * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            playerMove = -speed * Time.deltaTime;
+        }
+
+        this.transform.Translate(new Vector3(playerMove, 0, 0));
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
     }
 }
